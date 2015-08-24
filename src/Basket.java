bf45c1c8f7b56ddbc7d375768c406058c5745832
anshup <anshup@INanshup.local>
2015-08-24 18:20:18 +0530

    Adding a basket class

---
+++ src/Basket.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * Created by anshup on 24/08/15.
+ */
+public class Basket {
+    ArrayList<Goods> basket = new ArrayList<Goods>();
+
+    public Basket(ArrayList<Goods> basket) {
+        this.basket = basket;
+    }
+    public Basket(){
+
+    }
+}
--- src/MainClass.java
+++ src/MainClass.java
@@ -2,15 +2,24 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by anshup on 24/08/15.
  */
 public class MainClass {
     public static void main(String args[])throws IOException{
+        String eachLine="";
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("INPUT");
-        System.out.println(br.readLine());
+        ArrayList<Basket> baskets = new ArrayList<Basket>();
+        System.out.println("What all things you bought today. Finish it by saying That is it");
+        while (!(eachLine=br.readLine()).equals("That is it")){
+            if (eachLine.contains("Input")){
+               Basket basket = new Basket();
+                continue;
+            }
+
+        }
 
     }
 }
